1d5c0bda350d2a1d42d56dd226feab996612aa16
sharnali <348486030@qq.com>
2020-07-16 20:06:14 +0800

    fix: commit

--- 练习-数据结构算法刷题/树/面试题34. 二叉树中和为某一值的路径.js
+++ 练习-数据结构算法刷题/树/面试题34. 二叉树中和为某一值的路径.js
@@ -1,3 +1,17 @@
-var pathSum = function (root, sum) {
-
+var pathSum = function(root, sum) {
+    if(!root) return [];
+    let paths=[];
+    _pathsum(root,sum,paths,[]);
+    return paths;
 };
+
+function _pathsum(root,sum,paths,path){
+    if(!root) return;
+    path=[...path,root.val];
+    if(!root.left&&!root.right&&root.val==sum){
+        paths.push(path);
+        return;
+    }
+    _pathsum(root.left,sum-root.val,paths,path);
+    _pathsum(root.right,sum-root.val,paths,path);
+}
--- 练习-数据结构算法刷题/递归/50.pow-x-n.js
+++ 练习-数据结构算法刷题/递归/50.pow-x-n.js
@@ -55,22 +55,12 @@
  * 时间复杂度：O(log⁡n) 递归树的高度 
  */
 var myPow = function (x, n) {
-  // 特殊处理
-  if (n >= 0) {
-    return quickPow(x, n)
-  }
-  // 特殊处理n为负
-  return 1 / quickPow(x, -n);
-
-  function quickPow(x, n) {
-    // base case;
-    if (n === 0) return 1;
-    // proccess
-    let _x = quickPow(x, Math.floor(n / 2));
-    // drill down
-    return n % 2 === 0 ? _x * _x : _x * _x * x
-    // restore
-  }
+  if(n===0) return 1; //n=0直接返回1
+    if(n<0) return 1/myPow(x,-n); //n<0时，x的n次方等于1除以x的-n次方
+    if(n%2){
+        return x*myPow(x,n-1); //n是奇数时，xdn次方=x*x的n-1次方
+    } 
+    return myPow(x*x,n/2); //n是偶数，使用分治，一分为二，等于x*x的n/2次方
 };
 // @lc code=end
 
